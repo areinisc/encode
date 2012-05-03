2a6d9db87d0135fe871cc65397de1fea139dbf64
Alex Reinisch <areinisc@slu.edu>
2012-05-03 16:50:37 -0500

    stuck

--- encode.cpp
+++ encode.cpp
@@ -14,38 +14,68 @@
 using namespace std;
 
 class mycomparison {
-    public:
-        mycomparison() {
-            //needs to compare frequencies, not pointers to trees
+    bool reverse;
+public:
+    mycomparison(const bool& revparam = false) {
+        reverse = revparam;
         }
+    bool operator()(const int& lhs, const int& rhs) {
+        if (reverse)
+            return lhs > rhs;
+        else
+            return lhs < rhs;
+    }
 };
 
 int bits_per_pattern(8);
 int characterFrequency[255];
 string filename;
 InBitStream inStream;
-priority_queue< LinkedBinaryTree<int>*, vector<int>, mycomparison> frequencyQueue;
+priority_queue< pair<LinkedBinaryTree<int>*, int>, vector<int>, mycomparison> frequencyQueue;
 
-void charFreq() {
+/*
+ * charFreq() opens the user specified input file
+ * reads in $bits_per_pattern$ bits at a time
+ * positively increments each corresponding entry of array: characterFrequency
+ * closes the input file
+ */
+void charFreq() {                                       // fulfills task 1
     cout << "Name the file to be compressed:\t";
     cin >> filename;
     inStream.open(filename);
     while (!inStream.eof()) {
         int temp = inStream.read(bits_per_pattern); 
-        ++characterFrequency[temp]; // counts frequency positively 
+        ++characterFrequency[temp];                     // counts frequency positively 
         // ***********************come back here later for if odd number of bytes
     }
     inStream.close();
 }
 
+/*
+ * buildTree() uses pg.577
+ * makes a priority queue of pairs
+ * pairs of pointers to LinkedBinaryTrees and frequency counts
+ * join minimum frequency trees, reinsert to queue, repeat
+ */
 void buildTree() {
     for (int a = 0; a < 256; a++) {
         if (characterFrequency[a] != 0) {
-            //LinkedBinaryTree<int> // create tree
-            // push tree into frequencyQueue
+            LinkedBinaryTree<int> *lbt;
+            lbt = new LinkedBinaryTree<int>();
+            *lbt.addRoot(a);
+            pair <LinkedBinaryTree<int>*, int> pr (lbt, characterFrequency[a]); // create pair of pointer to LBT and frequency
+            frequencyQueue.push(pr); // insert pair into pqueue
         }
     }
+    while (frequencyQueue.size() > 1) {
+        // make a new root pointer
+        // expand root
+        // join 2 minimum from priority queue as subtrees
+    }
 }
+
+// precompute huff codes by traversing tree
+
 
 int main() {
     return 0;
